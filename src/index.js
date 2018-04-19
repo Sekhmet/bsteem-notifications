4852de66745f17bbe07083197acbaf741651c6b2
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2018-04-19 23:16:14 +0200

    feat: authenticate user

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const _ = require('lodash');
+const Bluebird = require('bluebird');
 const express = require('express');
 const bodyParser = require('body-parser');
 const Expo = require('expo-server-sdk');
+const sc2 = require('sc2-sdk');
 const { createQueue } = require('./queue');
 const fetcher = require('./fetcher');
 const initializer = require('./initializer');
@@ -21,20 +23,41 @@ async function start() {
   app.post('/register', async (req, res) => {
     const token = _.get(req, 'body.token');
     const username = _.get(req, 'body.username');
+    const accessToken = _.get(req, 'body.accessToken');
 
-    if (!token || !username || !Expo.isExpoPushToken(token) || typeof username !== 'string') {
+    if (
+      !token ||
+      !username ||
+      !accessToken ||
+      !Expo.isExpoPushToken(token) ||
+      typeof username !== 'string' ||
+      typeof accessToken !== 'string'
+    ) {
       return res.status(400).send({
-        error: 'valid token and username are required',
+        error: 'valid token, username, and accessToken are required',
       });
     }
 
     try {
+      const api = sc2.Initialize({
+        app: 'busy.app',
+      });
+      api.setAccessToken(accessToken);
+
+      const me = Bluebird.promisify(api.me, { context: api });
+
+      const user = await me();
+
+      if (_.get(user, 'name') !== username) {
+        return res.sendStatus(401);
+      }
+
       await queue.registerUser(username, token);
       return res.send({
         message: 'registered',
       });
     } catch (err) {
-      return res.sendStatus(501);
+      return res.sendStatus(500);
     }
   });
 
