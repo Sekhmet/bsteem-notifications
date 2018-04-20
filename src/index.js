0d428960639311fe404adf8b73193113d45aa5e0
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2018-04-20 18:29:08 +0200

    feat: set MONGODB_URI

--- src/index.js
+++ src/index.js
@@ -6,13 +6,14 @@ const fetcher = require('./fetcher');
 const initializer = require('./initializer');
 const router = require('./routes');
 
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/bsteem';
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 app.use(bodyParser.json());
 
 async function start() {
-  mongoose.connect('mongodb://localhost/bsteem');
+  mongoose.connect(MONGODB_URI);
   const queue = await createQueue();
 
   fetcher(queue);
