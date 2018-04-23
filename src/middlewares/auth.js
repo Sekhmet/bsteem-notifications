a3dd5b2faffdde8e3a1719d1e3606c79d6ac7c72
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2018-04-24 00:34:38 +0200

    feat: add timeout to auth middleware

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,6 +1,15 @@
 const Bluebird = require('bluebird');
 const sc2 = require('sc2-sdk');
 
+function createTimeout(timeout, promise) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      reject(new Error(`Request has timed out. It should take no longer than ${timeout}ms.`));
+    }, timeout);
+    promise.then(resolve, reject);
+  });
+}
+
 async function authMiddleware(req, res, next) {
   const token = req.get('Authorization');
   if (!token) {
@@ -16,7 +25,7 @@ async function authMiddleware(req, res, next) {
 
     const me = Bluebird.promisify(api.me, { context: api });
 
-    const user = await me();
+    const user = await createTimeout(10000, me());
 
     if (!user) {
       return res.sendStatus(401);
